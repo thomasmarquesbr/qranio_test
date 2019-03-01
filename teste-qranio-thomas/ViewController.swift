a1be1f0485b414e19e1ee78f6ea8314d6fe087c8
Thomas <msamoht16@gmail.com>
2019-03-01 13:10:37 -0300

    Initial Commit

---
+++ teste-qranio-thomas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teste-qranio-thomas
+//
+//  Created by Thomás Marques Brandão Reis on 01/03/19.
+//  Copyright © 2019 Thomás Marques Brandão Reis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ teste-qranio-thomasTests/teste_qranio_thomasTests.swift
@@ -0,0 +1,34 @@
+//
+//  teste_qranio_thomasTests.swift
+//  teste-qranio-thomasTests
+//
+//  Created by Thomás Marques Brandão Reis on 01/03/19.
+//  Copyright © 2019 Thomás Marques Brandão Reis. All rights reserved.
+//
+
+import XCTest
+@testable import teste_qranio_thomas
+
+class teste_qranio_thomasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
